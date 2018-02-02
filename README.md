ecb5e8c5b8894bc9de191b78c0b676b44f69205c
FrankLee <frank25184@sina.com>
2018-02-02 12:26:08 +0800

    add ngnix

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Nodeclub
 [node-url]: http://nodejs.org/download/
 
 ## 介绍
-
+English verson of Nodeclub
 Nodeclub 是使用 **Node.js** 和 **MongoDB** 开发的社区系统，界面优雅，功能丰富，小巧迅速，
 已在Node.js 中文技术社区 [CNode(http://cnodejs.org)](http://cnodejs.org) 得到应用，但你完全可以用它搭建自己的社区。
 
--- config.js
+++ config.js
@@ -30,7 +30,7 @@ var config = {
   // cdn host，如 http://cnodejs.qiniudn.com
   site_static_host: '', // 静态文件存储域名
   // 社区的域名
-  host: 'localhost:3000',
+  host: '2hours.cc',
   // 默认的Google tracker ID，自有站点请修改，申请地址：http://www.google.com/analytics/
   google_tracker_id: '',
   // 默认的cnzz tracker ID，自有站点请修改
